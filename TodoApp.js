980d75fbcd228b27f758b408aae7a89f32d298e4
nagydan1 <daniel.nagy.w@gmail.com>
2021-11-24 20:00:44 +0100

    argument error and delete

--- TodoApp.js
+++ TodoApp.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import Todo from "./TodoClass.js";
 
 export default class TodoApp {
     #args;
@@ -19,30 +20,54 @@ export default class TodoApp {
         } else {
             for (let k in parsedList) {
                 console.log(
-                    `${parseInt(k) + 1} - ${parsedList[k]}`
+                    `${parseInt(k) + 1} - ${parsedList[k]["todo"]}`
                 )
             }
         }
     }
 
     addTodo(newTodo) {
+        let item = new Todo(newTodo);
         let parsedList = this.getParsedList();
-        parsedList.push(newTodo);
-        let list = JSON.stringify(parsedList);
-        fs.writeFileSync("./Data/todos.json", list);
+        parsedList.push(item);
+        writeData(parsedList);
+    }
+
+    markDone(doneIndex) {
+        let parsedList = this.getParsedList();
+        parsedList[doneIndex - 1]["done"] = true;
+        writeData(parsedList);
+    }
+
+    delete(delIndex) {
+        let parsedList = this.getParsedList();
+        parsedList.splice(delIndex - 1, 1)
+        writeData(parsedList);
     }
 
     run() {
-
         if (this.#args.includes("-l")) {
             this.printList();
         } else if (this.#args.includes("-a")) {
             let newTodo = this.#args[1];
             this.addTodo(newTodo);
-        } else {
+        } else if (this.#args.includes("-c")) {
+            let doneIndex = this.#args[1];
+            this.markDone(doneIndex);
+        } else if (this.#args.includes("-r")) {
+            let delIndex = this.#args[1];
+            this.delete(delIndex);
+        } else if (this.#args.length == 0) {
             console.log(
                 fs.readFileSync("./intro.txt", "utf-8")
-            );
-        };
+            )
+        } else {
+            console.log("Nem támogatott argumentum!")
+        }
     }
+}
+
+function writeData(input) {
+    let list = JSON.stringify(input);
+    fs.writeFileSync("./Data/todos.json", list);
 }
---
+++ TodoClass.js
@@ -0,0 +1,9 @@
+export default class Todo {
+    
+    constructor(todo) {
+        this.todo = todo;
+        this.done = false;
+    };
+
+}
+
