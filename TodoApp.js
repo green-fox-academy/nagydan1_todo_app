d4f4a8c4465ad68720b26bc1e589468bffc1bd6b
nagydan1 <daniel.nagy.w@gmail.com>
2021-11-24 19:17:59 +0100

    add todo done

--- TodoApp.js
+++ TodoApp.js
@@ -7,10 +7,13 @@ export default class TodoApp {
         this.#args = args;
     }
 
-    printList() {
+    getParsedList() {
         let list = fs.readFileSync("./Data/todos.json", "utf-8");
-        let parsedList = JSON.parse(list);
-        
+        return JSON.parse(list);
+    }
+
+    printList() {
+        let parsedList = this.getParsedList();
         if (parsedList.length == 0) {
             console.log(`Nincs mára tennivalód! :)`)
         } else {
@@ -22,10 +25,20 @@ export default class TodoApp {
         }
     }
 
+    addTodo(newTodo) {
+        let parsedList = this.getParsedList();
+        parsedList.push(newTodo);
+        let list = JSON.stringify(parsedList);
+        fs.writeFileSync("./Data/todos.json", list);
+    }
+
     run() {
 
         if (this.#args.includes("-l")) {
             this.printList();
+        } else if (this.#args.includes("-a")) {
+            let newTodo = this.#args[1];
+            this.addTodo(newTodo);
         } else {
             console.log(
                 fs.readFileSync("./intro.txt", "utf-8")
